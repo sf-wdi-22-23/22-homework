d0fc0a0191a6d95c157665afc368166c6ca30be7
FrancescaLong <francesca_long@hotmail.com>
2015-10-01 15:15:35 -0700

    fixed the buttons - they finally work

--- FrancescaLong/day3.js
+++ FrancescaLong/day3.js
@@ -1,28 +1,20 @@
 $(document).ready(function() {
 
-$("#b1").on("click", function() {
-       // change the image, and make sure this image path exists ...
-      $("#b1i").attr("src", "i20150806_071526-copy.jpg");
-      // make it disappear 
-      setTimeout(function(){
-        $("#b1i").hide();
-              },1000);
+$("#b1").on("click", function(){
+      $("#photo").hide(); 
   });
 
-$("#b2").on("click", function() {
-       // change the image, and make sure this image path exists ...
-      $("#b2i").attr("src", "i20150806_071526-copy.jpg");
-      // make it appear 
-      setTimeout(function(){
-        $("#b2i").show();
-              },1000);
+
+
+$("#b2").on("click", function(){
+      $("#photo").show(); 
   });
+
 
 $("#b3").on("click", function() {
-       // change the image, and make sure this image path exists ...
-      $("#b2i")."p";
-      // change the color 
-      color: blue;
-  });
+       $("#paragraph").css('color', 'green'); 
+//       $("#paragraph").style(color=green); 
+   });
+   
 
 });
