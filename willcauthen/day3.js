067f2c2caf12a701bee4b5ae942259604c25300d
goodbedford <bedford@goodbedford.com>
2015-10-05 11:56:21 -0700

    fix merge

--- willcauthen/day3.js
+++ willcauthen/day3.js
@@ -1,20 +1,22 @@
 $('document').ready(function() { 
-	$('button1').onClick(){
-		$('img').hide();
-	};
-});
+	
+	$( "#button1" ).click(function() {
+  $( "#barney" ).fadeOut( "slow" );
+		});
 
 
-$('document').ready(function() {
-	$('button2').onClick() {
-			$('img').show();
-	}
 
-});
 
-$('document').ready(function() {
-	$('button3').onClick() {
+	$('#button2').click(function() {
+			$('#barney').fadeIn('slow');
+	
+		});
 
-	}
+
+
+	$('#button3').click(function() {
+		$('#latin').html("Banjo raw denim letterpress Echo Park. Kale chips Odd Future master cleanse, fap actually lo-fi roof party. Fap jean shorts twee, PBR 8-bit VHS selvage organic cronut tofu vegan slow-carb retro master cleanse. Swag Bushwick Vice trust fund banjo. Plaid post-ironic forage, +1 tilde flexitarian cray. Asymmetrical Wes Anderson organic, street art letterpress food truck hashtag synth Kickstarter ethical Brooklyn skateboard biodiesel put a bird on it. Four dollar toast gluten-free iPhone Tumblr.");
+
+		});
 });
 
