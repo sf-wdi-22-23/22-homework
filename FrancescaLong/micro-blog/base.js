cdd04a0c64ef58810f153278a058ce191d0197cb
FrancescaLong <francesca_long@hotmail.com>
2015-10-10 00:41:05 -0700

    working on grabbing the text

--- FrancescaLong/micro-blog/base.js
+++ FrancescaLong/micro-blog/base.js
@@ -2,6 +2,38 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+/* variables used in the code */
+  var posts = {
+  	input: "",
+  	counter: 0,
+  	postDateTime: ""
+  };
+
+$("#submitButton").on("click", function(){
+	posts.push(
+  		input = "#inputBox",
+  		counter = counter++,
+  		postDateTime = new Date()
+  	  	);
+});
+
+console.log(posts());
+
+
+/*
+ $("#square1").one("click", function(){
+ 	$("#square1").css("color", "green");
+ 	$("#square1").append(switchMarker()); 
+ 	console.log("Play number " + turnCounter);
+ 	plays[0] = marker;
+ 	checkWinner();
+ 	if (winnerDeclared === 0) {
+ 		alert("Next player is " + nextPlayer);
+		}
+	});
+*/	
+
+
+
 
 });
