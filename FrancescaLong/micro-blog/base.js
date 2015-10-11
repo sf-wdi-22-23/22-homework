b211bab9d81033e8e043b05e908e016a783d79dd
FrancescaLong <francesca_long@hotmail.com>
2015-10-10 21:27:47 -0700

    still trying to get the data out of the form

--- FrancescaLong/micro-blog/base.js
+++ FrancescaLong/micro-blog/base.js
@@ -2,23 +2,47 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+var counter = 0;
+
 /* variables used in the code */
   var posts = {
   	input: "",
-  	counter: 0,
+  	counter: counter++,
   	postDateTime: ""
   };
 
-$("#submitButton").on("click", function(){
-	posts.push(
-  		input = "#inputBox",
-  		counter = counter++,
-  		postDateTime = new Date()
-  	  	);
-});
+posts.input = "test1",
+posts.counter = 1,
+posts.postDateTime = new Date()
 
-console.log(posts());
-console.log("test");
+
+/* internet solution 2 - also fails to put anyting into the object
+var data = $('#form').serializeArray().reduce(function(posts, input) {
+    posts[input.blog] = input.inputBox;
+    return posts;
+}, {});
+*/
+
+/* internet solution 1 - also fails to put anyting into the object
+$("#submitButton").on("click", function(){
+  		$("#form.input").each(function() {
+  			data[input] = input;
+  		 	data[counter] = counter++;
+  		 	data[postDateTime] = new Date();
+	});
+});
+*/
+
+/* My original solution - doesn't push the data into the object
+$("#submitButton").on("click", function(){
+  		posts.input = new input("#inputBox"),
+  		posts.counter = new counter(counter++),
+  		posts.postDateTime = new Date()
+});
+*/
+console.log(posts);
+console.log(data);
+alert(posts[0]);
 
 
 /*
