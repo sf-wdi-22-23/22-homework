239eea1063f5cc2e14f35b7367a61df819afcfc6
lauraevans99 <lauraevans99@gmail.com>
2015-10-12 03:31:40 -0700

    changed styling

--- lauraevans99/project0/base.js
+++ lauraevans99/project0/base.js
@@ -9,17 +9,30 @@ $(document).ready(function(){
 
   function App(){}
 
+  var numPosts = 0;
+
   App.prototype.addPost = function () {
+    var that = this;
   	$("#submit").on("click", function(e) {
   		var input = $("#input").val();
   		var ul = $("ul").append("<li class='list-group-item'>" + input + "<span class='glyphicon glyphicon-remove' aria-hidden='true'></span></li>");
   		console.log(ul);
+      numPosts++;
+      console.log(numPosts);
+      that.updateCounter();
   	});
   };
 
   App.prototype.deletePost = function () {
+    var that = this;
   	$("ul").on("click", function(e) {
   		console.log(this);
   		this.remove();
+      numPosts--;
+      that.updateCounter();
   	});
+  };
+
+  App.prototype.updateCounter = function () {
+    $("#counter").text("Total number of posts: " + numPosts);
   };
