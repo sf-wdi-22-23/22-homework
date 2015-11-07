8b071f161fe3bd8ef3a33c62b660c24b30997efd
Will Cauthen <willcauthen50@gmail.com>
2015-11-07 12:47:02 -0800

    bog app second commit

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -1,6 +1,25 @@
 class CreaturesController < ApplicationController
+	# show all the creatures
 	def index
 		@creatures = Creature.all
 		render :index
+	end
+	def new
+		@creature = Creature.new
+		render :new
+	end
+	def create
+		creature_params = params.require(:creature).permit(:name, :desciption)
+		# create a new creature with those parameters
+		creature = Creature.new(creature_params)
+		# check it was saved
+		if creature.save
+			redirect_to creature
+		end
+	end
+	def show
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :show
 	end
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,5 +1,6 @@
 # config routes for the thing
 Rails.application.routes.draw do
   root to: "creatures#index"
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
+  # get "/new" => "creatures#new"
 end
