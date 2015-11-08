2b62345213601c73cb6b23e037ad20334cd711d0
HollisDayCheer <hollisj@carleton.edu>
2015-11-08 15:36:39 -0800

    got create and show working

--- HollisDayCheer/bog_app/app/controllers/creatures_controller.rb
+++ HollisDayCheer/bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,22 @@ class CreaturesController < ApplicationController
 	def index
 		@creatures = Creature.all
 	end
+
+	def new
+		@creature = Creature.new
+	end
+	
+	def create 
+		creature_params = params.require('creature'.permit(:name, :description))
+
+		creature = Creature.new(creature_params)
+		if creature.save
+			redirect_to creatures_path
+		end
+	end
+
+	def show
+		id= params[:id]
+		@creature = Creature.find(id)
+	end
 end
