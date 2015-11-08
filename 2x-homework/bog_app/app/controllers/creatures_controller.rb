b1fd46ff12dafb9afae25ceb4bd1ba8c4aa2f061
bgabot89 <bgabot89@gmail.com>
2015-11-07 22:48:21 -0800

    rails lab

---
+++ 2x-homework/bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,9 @@
+cclass CreaturesController < ApplicationController
+    # show all creatures
+    def index
+        # get all creatures from db and save to instance variable
+        @creatures = Creature.all  
+        # render index view file (it will have access to instance variables)
+        render :index
+    end
+end
