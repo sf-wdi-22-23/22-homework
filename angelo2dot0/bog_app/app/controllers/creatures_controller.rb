13d08b25f07e93ef7d4f6fac5ca026cb65a4ce2d
angelo2dot0 <angeloaramayo@gmail.com>
2015-11-08 15:09:40 -0800

    bog_app - adds part two

--- angelo2dot0/bog_app/app/controllers/creatures_controller.rb
+++ angelo2dot0/bog_app/app/controllers/creatures_controller.rb
@@ -5,4 +5,36 @@ class CreaturesController < ApplicationController
 		# get all creatures from db and save to instance variable
 		# render index view file (it will have access to instance variables)
 	end
+
+    def new
+    	@creature = Creature.new
+        render :new   # optional; this is the default behavior
+        # show the new creature form
+    end
+
+    # create a new creature in the database
+    def create
+        # validate params and save them as a variable
+        creature_params = params.require(:creature).permit(:name, :description)
+        # create a new creature with those params
+        creature = Creature.new(creature_params)
+        # check that it saved
+        if creature.save
+            # if saved, redirect to route that shows just this creature
+            redirect_to creature
+            # ^ same as redirect_to creature_path(creature)
+            # ^ same as redirect_to "/creatures/#{creature.id}"
+        end
+    end
+
+    # show a single creature
+    def show
+        # get the id parameter from the url
+        id = params[:id]
+        # find the creature with that id and save to an instance variable
+        @creature = Creature.find(id)
+        # render the show page -- it will have access to the @creature variable
+        render :show
+    end
 end
+
--- angelo2dot0/bog_app/config/routes.rb
+++ angelo2dot0/bog_app/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   root to: 'creatures#index'
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
   # resources :creatures with :index is equivalent to adding:
     # get "/creatures", to: "creatures#index"
-
-  
-  
-
-
-
-
+  # resources :creatures with :new is equivalent to adding:
+    # get "/creatures/new", to: "creatures#new", as: "new_creature"
+  # resources :creatures with :create is equivalent to adding:
+    # post "/creatures", to: "creatures#create"
+  # resources :creatures with :show is equivalent to adding:
+    # get "/creatures/:id", to: "creatures#show", as: "creature"
 end
+
