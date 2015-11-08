67808ab17fffd8229e3135072bb1334cfb84b1dc
FrancescaLong <francesca_long@hotmail.com>
2015-11-07 22:45:10 -0800

    create for done, show individual creature pages created

--- FrancescaLong/bog_app/config/routes.rb
+++ FrancescaLong/bog_app/config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-
   root to: 'creatures#index'
 
   # We'll also use the resources method to have Rails 
   # make an index route for our creatures resource.
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
   # resources :creatures with :index is equivalent to adding:
   # get "/creatures", to: "creatures#index"
 
+  # resources :creatures with :new is equivalent to adding:
+  # get "/creatures/new", to: "creatures#new", as: "new_creature"
+
+  # resources :creatures with :create is equivalent to adding:
+  # post "/creatures", to: "creatures#create"
+
+  # resources :creatures with :show is equivalent to adding:
+  # get "/creatures/:id", to: "creatures#show", as: "creature"
 end
