d4626ceffe7178fa010a2e8a1c15c9c9124515ef
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-11-08 13:05:39 -0800

    update creatures

--- kizersam/bog_app/app/controllers/creatures_controller.rb
+++ kizersam/bog_app/app/controllers/creatures_controller.rb
@@ -30,4 +30,27 @@ class CreaturesController < ApplicationController
 		render :show
 	end
 
+	def edit
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
+	end
+
+	 def update
+        # save the id paramater from the url
+        creature_id = params[:id]
+        # find the creature to update by id
+        creature = Creature.find(creature_id)
+
+        # get updated creature data from params
+        updated_attributes = params.require(:creature).permit(:name, :description)
+        # update the creature
+        creature.update_attributes(updated_attributes)
+
+        # redirect to single creature show page for this creature
+        redirect_to creature
+        # ^ same as redirect_to creature_path(creature)
+        # ^ same as redirect_to "/creatures/#{creature.id}"
+    end
+
 end
--- kizersam/bog_app/config/routes.rb
+++ kizersam/bog_app/config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   
   # We'll also use the resources method to have Rails 
     # make an index route for our creatures resource.
-    resources :creatures, only: [:index, :new, :create, :show]
+    resources :creatures, only: [:index, :new, :create, :show, :edit, :update]
     # resources :creatures with :index is equivalent to adding:
     # get "/creatures", to: "creatures#index"
      # resources :creatures with :new is equivalent to adding:
     # get "/creatures/new", to: "creatures#new", as: "new_creature"
     # resources :creatures with :create is equivalent to adding:
     # post "/creatures", to: "creatures#create"
+      # resources :creatures with :update is equivalent to adding BOTH:
+    # patch "/creatures/:id", to: "creatures#update"
+    # AND
+    # put "/creatures/:id", to: "creatures#update"
 end
