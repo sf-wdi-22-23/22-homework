6ba65e5d54cbe8b2725abfee4ee50c3c05f1b723
Uriel <uriel.chavez@gmail.com>
2015-11-07 17:46:03 -0800

    new creature routes and params made

--- TannedAlbino/bog_app/app/controllers/creatures_controller.rb
+++ TannedAlbino/bog_app/app/controllers/creatures_controller.rb
@@ -5,4 +5,33 @@ class CreaturesController < ApplicationController
 		render :index
 	end
 
+	# show the new creature form
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	# create a new creature in the database
+	def create
+		# validate params and save them as a variable
+		creature_params = params.require(:creature).permit(:name, :description)
+		# create a new creature with those params
+		creature = Creature.new(creature_params)
+		# check that it saved
+		if creature.save
+			# if saved, redirect to route that shows all creatures note: following is same as redirect_to "/creatures"
+			redirect_to creature
+		end
+	end
+
+	def show
+		# get the id parameter from the url
+		id = params[:id]
+		# find the creature with that id and save to an instance variable
+		@creature = Creature.find(id)
+		# render the show page - it will have access to the @creature variable
+		render :show
+	end
+
+
 end
