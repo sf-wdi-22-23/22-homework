3c7fd2b85370fa6faddaafda002753d8fc3b15e8
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-11-08 19:39:08 -0800

    added create route and views etc.

--- krustacean/bog_app/app/controllers/creatures_controller.rb
+++ krustacean/bog_app/app/controllers/creatures_controller.rb
@@ -1,6 +1,19 @@
 class CreaturesController < ApplicationController
   def index
-       @creatures = Creature.all
-      render :index
-   end
+    @creatures = Creature.all
+    render :index
+  end
+  def new
+  end
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+     # create a new creature with those params
+     creature = Creature.new(creature_params)
+     # check that it saved
+     if creature.save
+         # if saved, redirect to route that shows all creatures
+         redirect_to creatures_path
+         # ^ same as redirect_to "/creatures"
+     end
+  end
 end
--- krustacean/bog_app/config/routes.rb
+++ krustacean/bog_app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     root to: 'creatures#index'
 
-    resources :creatures, only: [:index]
+    resources :creatures, only: [:index, :new, :create]
 
 
 
