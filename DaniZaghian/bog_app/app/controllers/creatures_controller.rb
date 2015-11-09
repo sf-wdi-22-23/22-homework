b40f732526bf834d053f987f4fbc8750d46d7d11
danielle <dani.zaghian@gmail.com>
2015-11-08 19:59:45 -0800

    added creatures create and show methods, added views

--- DaniZaghian/bog_app/app/controllers/creatures_controller.rb
+++ DaniZaghian/bog_app/app/controllers/creatures_controller.rb
@@ -6,4 +6,36 @@ class CreaturesController < ApplicationController
         # render index view file (it will have access to instance variables)
         render :index
     end
+
+     # show the new creature form
+    def new
+        @creature = Creature.new
+        render :new
+    end
+
+    # create a new creature in the database
+    def create
+        # validate params and save them as a variable
+        creature_params = params.require(:creature).permit(:name, :description)
+        # create a new creature with those params
+        creature = Creature.new(creature_params)
+        # check that it saved
+        if creature.save
+            # if saved, redirect to route that shows just this creature
+            redirect_to creature
+            # ^ same as redirect_to creature_path(creature)
+            # ^ same as redirect_to "/creatures/#{creature.id}"
+        end
+    end
+
+    # show a single creature
+    def show
+        # get the id parameter from the url
+        id = params[:id]
+        # find the creature with that id and save to an instance variable
+        @creature = Creature.find(id)
+        # render the show page -- it will have access to the @creature variable
+        render :show
+    end
+
 end
