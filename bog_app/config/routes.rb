1a828baa99d39d4510c391a905b25fef7c60239f
Will Cauthen <willcauthen50@gmail.com>
2015-11-09 13:06:04 -0800

    m

--- bog_app/config/routes.rb
+++
@@ -1,7 +0,0 @@
-# config routes for the thing
-Rails.application.routes.draw do
-  root to: "creatures#index"
-  resources :creatures
-  # resources :creatures, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  # get "/new" => "creatures#new"
-end
