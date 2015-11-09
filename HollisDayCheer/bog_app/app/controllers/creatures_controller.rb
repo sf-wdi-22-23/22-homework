21ce3bfec5bc31bfacff886b03308bde76caee43
HollisDayCheer <hollisj@carleton.edu>
2015-11-08 18:59:57 -0800

    finished lab

--- HollisDayCheer/bog_app/app/controllers/creatures_controller.rb
+++ HollisDayCheer/bog_app/app/controllers/creatures_controller.rb
@@ -13,12 +13,33 @@ class CreaturesController < ApplicationController
 
 		creature = Creature.new(creature_params)
 		if creature.save
-			redirect_to creatures_path
+			redirect_to creature
 		end
 	end
 
 	def show
 		id= params[:id]
 		@creature = Creature.find(id)
+	end
+
+	def edit
+		id=params[:id]
+		@creature = Creature.find(id)
+	end
+
+	def update
+		id = params[:id]
+		creature = Creature.find(id)
+
+		updated_attributes = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(updated_attributes)
+		redirect_to creature
+	end
+
+	def destroy
+		id = params[:id]
+		creature = Creature.find(id)
+		creature.destroy
+		redirect_to creatures_path
 	end
 end
