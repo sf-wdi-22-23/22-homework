49cbc25db3223871cab9a25eb73d87bbd7b6d0ec
Laulaucia <buitrago.laura@gmail.com>
2015-11-09 07:30:11 -0800

    created lots of routes and views for creatures

--- Laulaucia/bog_app/app/controllers/creatures_controller.rb
+++ Laulaucia/bog_app/app/controllers/creatures_controller.rb
@@ -4,4 +4,21 @@ class CreaturesController < ApplicationController
 		@creatures = Creature.all
 		render :index
 	end
+	def new
+		@creature = Creature.new
+		render :new
+	end
+	def create
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature = Creature.new(creature_params)
+		if creature.save
+			#if saved redirect to all creatures
+			redirect_to creature
+		end
+	end
+	def show
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :show
+	end
 end
