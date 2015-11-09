3e3b38fb16ca7db4cb7e0935d1b2eac5185d307a
cd20202 <cd20202@live.com>
2015-11-08 18:48:54 -0800

    finished bog app functionality

--- cd20202/bog_app/app/controllers/creatures_controller.rb
+++ cd20202/bog_app/app/controllers/creatures_controller.rb
@@ -14,7 +14,7 @@ class CreaturesController < ApplicationController
 		creature_params = params.require(:creature).permit(:name, :description)
 		creature = Creature.create(creature_params)
 		if creature.save
-			redirect_to creatures_path
+			redirect_to creature
 		end
 	end
 
@@ -25,15 +25,24 @@ class CreaturesController < ApplicationController
 	end
 
 	def edit
-		
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
 	end
 
 	def update
-		
+		id = params[:id]
+		creature = Creature.find(id)
+		updated_info = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(updated_info)
+		redirect_to creature
 	end
 
 	def delete
-		 
+		id = params[:id]
+		creature = Creature.find(id)
+		creature.destroy
+		redirect_to creatures_path
 	end
 
 end
