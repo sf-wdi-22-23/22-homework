8a2c46ca80ed2ec0ecf90f27b3c8af9bd699e2ac
Matthew Cowles <mpcowles726@gmail.com>
2015-11-11 07:06:52 -0800

    up to showing creature after creating

---
+++ mpcowles726/bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,37 @@
+class CreaturesController < ApplicationController
+	def index
+		@creatures = Creature.all
+
+		render :index
+	end
+
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	def create
+
+		creature_params = params.require(:creature).permit(:name, :description)
+
+		creature = Creature.new(creature_params)
+
+		if creature.save
+
+			redirect_to creature
+		end
+	end
+
+	def show
+	
+		id = params[:id]
+
+		@creature = Creature.find(id)
+
+		render :show
+	end
+
+
+
+
+end
---
+++ mpcowles726/bog_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+root to: 'creatures#index'
+
+resources :creatures, only: [:index, :new, :create, :show]
+
+
+end
