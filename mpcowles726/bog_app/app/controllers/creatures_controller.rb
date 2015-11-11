db50b145552f9ae382415e88e0a3bb0a589c712c
Matthew Cowles <mpcowles726@gmail.com>
2015-11-11 07:17:45 -0800

    update feature added

--- mpcowles726/bog_app/app/controllers/creatures_controller.rb
+++ mpcowles726/bog_app/app/controllers/creatures_controller.rb
@@ -31,6 +31,26 @@ class CreaturesController < ApplicationController
 		render :show
 	end
 
+	def edit
+		id = params[:id]
+		@creature = Creature.find(id)
+
+		render :edit
+	end
+
+	def update
+		creature_id = params[:id]
+
+		creature = Creature.find(creature_id)
+
+		updated_attributes = params.require(:creature).permit(:name, :description)
+
+		creature.update_attributes(updated_attributes)
+
+		redirect_to creature
+
+	end
+
 
 
 
--- mpcowles726/bog_app/config/routes.rb
+++ mpcowles726/bog_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
 root to: 'creatures#index'
 
-resources :creatures, only: [:index, :new, :create, :show]
+resources :creatures, only: [:index, :new, :create, :show, :edit, :update]
 
 
 end
